6e74a88825a7412e589899903b88df490cd91ef3
jordansgrant <jordans.grant@gmail.com>
2023-12-05 21:55:49 -0700

    Finished day 3 part 2. That got out of hand fast. Solution: 73201705

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,24 +1,50 @@
 use std::fs;
+use std::ops::RangeInclusive;
 
-const ADJACENT_OFFSETS: &[(i32, i32)] = &[(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)];
+const ADJACENT_OFFSETS: &[(i32, i32)] = &[
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (1, 0),
+    (-1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+];
 
 fn is_symbol(val: u8) -> bool {
-    (33..=47).contains(&val) || (58..=64).contains(&val) || (91..=96).contains(&val) || (123..=126).contains(&val)
+    (33..=45).contains(&val)
+        || val == 47
+        || (58..=64).contains(&val)
+        || (91..=96).contains(&val)
+        || (123..=126).contains(&val)
 }
 
 fn is_digit(val: u8) -> bool {
     (48..=57).contains(&val)
 }
 
-fn add_offset(val: usize, offset: i32) -> Option<usize> {
-    if offset < 0 && val == 0 {
-        return None
-    }
-
-    Some(val + offset as usize)
+fn is_star(val: u8) -> bool {
+    val == 42
 }
 
-fn check_adjacency(engine: &Vec<Vec<u8>>, bounds: (usize, usize), location: (usize, usize)) -> bool {
+fn add_offset(val: usize, offset: i32) -> Option<usize> {
+    if offset < 0 && val == 0 {
+        return None;
+    }
+
+    if offset < 1 {
+        Some(val - offset.abs() as usize)
+    } else {
+        Some(val + offset as usize)
+    }
+}
+
+fn check_adjacency_part_number(
+    engine: &Vec<Vec<u8>>,
+    bounds: (usize, usize),
+    location: (usize, usize),
+) -> bool {
     ADJACENT_OFFSETS.iter().fold(false, |acc, &offset| {
         let Some(x_offset) = add_offset(location.0, offset.0) else { return acc || false };
         let Some(y_offset) = add_offset(location.1, offset.1) else { return acc || false };
@@ -31,18 +57,82 @@ fn check_adjacency(engine: &Vec<Vec<u8>>, bounds: (usize, usize), location: (usi
     })
 }
 
-// returns a tuple of the parsed value and the index of the last digit
-fn parse_number(engine_row: &Vec<u8>, width: usize, index: usize) -> (u32, usize) {
-    let mut first = index;
-    let mut last = index;
-    while first > 0 && is_digit(engine_row[first]) {
-        first -= 1;
-    }
-    while (last < width && is_digit(engine_row[last])) {
-        last = index + 1;
+fn create_range(index1: usize, index2: usize) -> RangeInclusive<usize> {
+    if index1 > index2 {
+        return index2..=index1;
     }
 
-    (engine_row[(first + 1)..last].parse::<u32>().unwrap(), last - 1)
+    index1..=index2
+}
+
+fn new_gear_value(
+    engine: &Vec<Vec<u8>>,
+    acc: (u32, Option<Vec<usize>>),
+    location: (usize, usize),
+) -> (u32, Option<Vec<usize>>) {
+    if acc.0 == 0 {
+        return (1, Some(vec![location.0, location.1]));
+    }
+    if acc.0 == 2 {
+        return acc;
+    }
+
+    let positions: &Vec<usize> = &acc.1.as_ref().unwrap();
+    if positions[1] == location.1
+        && engine[location.1][create_range(positions[0], location.0)]
+            .iter()
+            .all(|&b| is_digit(b))
+    {
+        return acc;
+    }
+
+    return (
+        2,
+        Some(vec![positions[0], positions[1], location.0, location.1]),
+    );
+}
+
+fn adjacency_gear_ratio(
+    engine: &Vec<Vec<u8>>,
+    bounds: (usize, usize),
+    location: (usize, usize),
+) -> (bool, Option<Vec<usize>>) {
+    let (count, maybe_vec) = ADJACENT_OFFSETS.iter().fold((0, None), |acc, &offset| {
+        let Some(x_offset) = add_offset(location.0, offset.0) else { return acc };
+        let Some(y_offset) = add_offset(location.1, offset.1) else { return acc };
+
+        if x_offset > bounds.0 || y_offset > bounds.1 {
+            return acc;
+        }
+
+        if is_digit(engine[y_offset][x_offset]) {
+            return new_gear_value(engine, acc, (x_offset, y_offset));
+        }
+
+        return acc;
+    });
+
+    if count == 2 {
+        return (true, maybe_vec);
+    }
+
+    return (false, None);
+}
+
+// returns a tuple of the parsed value and the index of the last digit
+fn parse_number(engine_row: &Vec<u8>, len: usize, index: usize) -> (u32, usize) {
+    let mut first = index;
+    let mut last = index;
+    while first > 0 && is_digit(engine_row[first - 1]) {
+        first -= 1;
+    }
+    while last < len && is_digit(engine_row[last + 1]) {
+        last += 1;
+    }
+
+    let string = std::str::from_utf8(&engine_row[first..=last]).unwrap();
+    let number = string.parse::<u32>().unwrap();
+    return (number, last);
 }
 
 fn main() {
@@ -56,18 +146,38 @@ fn main() {
     let width = engine[0].len() - 1;
     let height = engine.len() - 1;
 
+    // Part 1
     let mut count = 0;
-
     for j in 0..=height {
         let mut width_iter = 0..=width;
-        while let Some(i) = width_iter.next()  {
-            if is_digit(engine[j][i]) && check_adjacency(&engine, (width, height), (i, j)) {
+        while let Some(i) = width_iter.next() {
+            if is_digit(engine[j][i])
+                && check_adjacency_part_number(&engine, (width, height), (i, j))
+            {
                 let (number, last_digit) = parse_number(&engine[j], width, i);
                 count += number;
-                width_iter.nth(last_digit);
+                width_iter.nth(last_digit - i);
             }
         }
     }
+    println!("{}", count);
 
-    println!("{:?}", count);
+    // Part 2
+    let mut count = 0;
+    for j in 0..=height {
+        let mut width_iter = 0..=width;
+        while let Some(i) = width_iter.next() {
+            if is_star(engine[j][i]) {
+                let (is_gear_ratio, maybe_positions) =
+                    adjacency_gear_ratio(&engine, (width, height), (i, j));
+                if is_gear_ratio {
+                    let positions = maybe_positions.unwrap();
+                    let (number1, _) = parse_number(&engine[positions[1]], width, positions[0]);
+                    let (number2, _) = parse_number(&engine[positions[3]], width, positions[2]);
+                    count += number1 * number2;
+                }
+            }
+        }
+    }
+    println!("{}", count);
 }
