555d73bdf74d41851b30b72ec135ed0bf7ba4b66
jordansgrant <jordans.grant@gmail.com>
2023-12-12 23:57:08 -0700

    Solve day 9 part 1. Solution: 1479011877

---
+++ day_9/src/main.rs
@@ -0,0 +1,30 @@
+use itertools::Itertools;
+use std::fs;
+
+fn parse_sequence(line: &str) -> Vec<i32> {
+    line.split_whitespace()
+        .filter_map(|s| s.parse().ok())
+        .collect()
+}
+
+fn find_next_val(seq: &Vec<i32>, acc: i32) -> i32 {
+    if seq.iter().all(|&v| v == 0) {
+        return acc;
+    }
+
+    let diffs: Vec<i32> = seq.iter().tuple_windows().map(|(l, r)| r - l).collect();
+    return find_next_val(&diffs, seq.iter().last().unwrap() + acc);
+}
+
+fn part_1(seq: &Vec<Vec<i32>>) -> i32 {
+    seq.iter()
+        .map(|s| find_next_val(s, 0))
+        .fold(0, |acc, v| acc + v)
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Failed to open file");
+    let sequences: Vec<Vec<i32>> = input.lines().map(|line| parse_sequence(line)).collect();
+
+    println!("{}", part_1(&sequences));
+}
